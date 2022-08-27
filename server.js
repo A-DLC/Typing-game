0fa8bc9a27e2fa988ae789bc314c8de83c614026
A-DLC <larrealangel712@gmail.com>
2022-08-26 23:09:51 -0400

    initial commit

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express(); //express as
 
 const PORT = process.env.PORT || 3000; //USAR PUERTO ENCONTRADO, O SINO EL PUERTO 3000
 
+app.set("view engine", "ejs");
+
+//home route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(PORT, console.log("server online"));
